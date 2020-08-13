00c6644fd255dc66227f74c909b9fc6ae3d8abc5
Mark Kelly <big.mako@gmail.com>
2020-08-13 02:22:19 -0700

    changed append to html

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get("/api/user_data").then(function(data) {
-      $(".member-name").text(data.email);
-      $(".member-name").text(data.username);
-    });
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function(data) {
+    $(".username").text(data.username);
+    $(".userEmail").text(data.email);
+    $(".userPassword").text(data.password);
   });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elem = document.querySelector('.collapsible.popout');
+  var instance = M.Collapsible.init(elem, {
+  accordion: false
+});
+});
