9e5e85867a866cb4801384bf8a8e6844756b45e5
Weerklank <63619983+Weerklank@users.noreply.github.com>
2020-08-15 11:56:27 -0700

    console logging out data to see what it's returning

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
+    console.log(data)
     $(".username").text(data.username);
     $(".userEmail").text(data.email);
     $(".userPassword").text(data.password);
