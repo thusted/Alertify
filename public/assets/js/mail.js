4e3d43b2460a50fd39b533c3a12f5701d23060e5
Weerklank <Zoe.veggie@gmail.com>
2020-08-17 00:02:08 -0700

    Added contact functionality

---
+++ public/assets/js/mail.js
@@ -0,0 +1,9 @@
+document.getElementById("emailSubmit").addEventListener("click", function (e) {
+    e.preventDefault()
+    var name = $("input#nameInput").val();
+    var subject = $("input#subjectInput").val();
+    var body = document.getElementById("msgInput").value;
+    var msg = `${body}%0d%0a%0d%0aRegards, ${name}`
+    console.log(subject,msg)
+    window.open(`mailto:teamalertify@gmail.com?subject=${subject}&body=${msg}`);
+});
