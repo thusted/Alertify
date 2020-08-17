749ec1c072f3585195e3a6b8e459a4183a07c28b
MarkKelly <35087238+MarkKelly00@users.noreply.github.com>
2020-08-16 22:52:42 -0700

    Update members.js

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -7,45 +7,56 @@ $(document).ready(function () {
     } else {
       $(".username").text(data.username);
     }
-  });
-
-  $.get("/api/login").then(function (data) {
+  $.get("/api/login").then(function(data) {
     console.log(data);
     $(".username").text(data.username);
     $(".userEmail").text(data.email);
     $(".userPassword").text(data.password);
-    // const contacts = await db.Contacts.findOne({
-    //   where: {
-    //     UserId: data.id
-    //   }
-    // });
-    $.get("/api/contacts").then(function (req) {
-      const contacts = req.filter((x) => (x.id = data.id));
-      console.log(contacts);
-      $(".userContacts").text(
-        `<ul><li>${contacts[0].name}:${contacts[0].phone}</li><li>${contacts[1].name}:${contacts[1].phone}</li><li>${contacts[2].name}:${contacts[2].phone}</li></ul>`
-      );
+    $(".userContacts").text(`${data.iceName}:${data.icePhone}`);
+  });
+});
+
+$(".submit-button").on("click", function (event) {
+  $.get("/api/user_data").then(function (data) {
+    event.preventDefault();
+    $.ajax({
+      url: "/api/user_data",
+      type: "PUT",
+      data: {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        username: data.username,
+        email: data.email,
+        iceName: data.iceName,
+        icePhone: data.icePhone,
+        password: $("#passwordUpdateCurrent")[0].value,
+        newPassword: $("#passwordUpdateNew")[0].value,
+      }
     });
   });
+});
 
-  $(".submit-button").on("click", function (event) {
-    $.get("/api/user_data").then(function (data) {
-      event.preventDefault();
-      $.ajax({
-        url: "/api/user_data",
-        type: "PUT",
-        data: {
-          firstName: data.firstName,
-          lastName: data.lastName,
-          username: data.username,
-          email: data.email,
-          password: $("#passwordUpdateCurrent")[0].value,
-          newPassword: $("#passwordUpdateNew")[0].value,
-        },
-      });
-    });
-  });
+// $(".contact-button").on("click", function (event) {
+//   $.get("/api/user_data").then(function (data) {
+//     event.preventDefault();
+//     $.ajax({
+//       url: "/api/user_data",
+//       type: "PUT",
+//       data: {
+//         firstName: data.firstName,
+//         lastName: data.lastName,
+//         username: data.username,
+//         email: data.email,
+//         iceName: $("#contactUpdateName")[0].value,
+//         icePhone: $("#contactUpdateNumber")[0].value,
+//         password: $("#passwordUpdate")[0].value,
+//       }
+//     });
+//   });
+// });
 
+
+document.addEventListener("DOMContentLoaded", function () {
   var elem = document.querySelector(".collapsible.popout");
   M.Collapsible.init(elem, {
     accordion: false,
